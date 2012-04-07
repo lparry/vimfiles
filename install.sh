b1fd9dc151e18584a10406b22fdd7a1cb2bd44e2
Lucas Parry <lucas@envato.com>
2012-04-07 12:22:12 +1000

    update installer

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 rm -rf ~/.vimrc ~/.vim
 ln -s ~/Dropbox/vimfiles/vim ~/.vim
 ln -s ~/Dropbox/vimfiles/vimrc ~/.vimrc
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim -c "execute \"BundleInstall\" | q | q"
